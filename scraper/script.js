5b8dfd18ce01e1d107d9fd0742cfad1cf4347a68
Kazoolund <christian.olund2@hotmail.com>
2020-04-06 09:20:40 +0200

    Wololo

--- scraper/script.js
+++ scraper/script.js
@@ -1,1 +1,18 @@
+"use strict";
+
 console.log("Scraper");
+let test ={
+    url: "https://www.hltv.org/stats/teams/6665/Astralis",
+    location: "div.columns:nth-child(5) > div.col:nth-child(2) > div.large-strong",
+    handle: element => {console.log(element)},
+}
+
+async function run_scraper(scraper){
+    let response = await fetch(scraper.url);
+    if (response.status == 200){
+        let html = await response.text();
+        let domparser = new DOMParser();
+        let dom = domparser.parseFromString(html, "text/html");
+        console.log(dom);
+    }
+}
