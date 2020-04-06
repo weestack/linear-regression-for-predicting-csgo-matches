33ce05b6b8449d4f461e785363ac95e181174608
Weestack <Alexander@helius.dk>
2020-04-06 14:12:31 +0200

    added function for calculating coeficcients

--- prediction_lib/regression.js
+++ prediction_lib/regression.js
@@ -11,14 +11,11 @@ const Linear_regression = () => {
         return sum/array.length
     };
 
-    const ss_xy = (x, y ) => {
+    const cross_deviation = (x, mean_x, y, mean_y ) => {
         /* Formula sum(x*y) - n*mean(x)*mean(y) */
         if (x.length !== y.length) {
             throw "x, and y should be of same length, error!"
         }
-
-        let mean_x = mean(x);
-        let mean_y = mean(y);
 
         let sum_multiply = 0;
 
@@ -30,15 +27,29 @@ const Linear_regression = () => {
 
     };
 
-    const ss_xx = (x) => {
-        let mean_x = mean(x);
-        let sum_multiply = 0;
 
-        for (let i=0; i < x.length; i++ ) {
-            sum_multiply += (x[i] * x[i])
-        }
+    const estimate_best_coeficcient = (x, y) => {
 
-        return sum_multiply - (x.length * mean_x * mean_x);
+        /* numeric Length of dataset */
+        let len = y.length;
+
+        /* Mean of both y and x */
+        let x_mean = mean(x);
+        let y_mean = mean(y);
+
+        /* Cross_deviation and deviation */
+        let ssxy = ss_xy(x, x_mean, y, y_mean);
+        let ssxx = ss_xy(x, x_mean, x, x_mean);
+
+        let intercept = ssxy / ssxx;
+        let slope = y_mean - intercept*x_mean;
+
+        /* slope corrosponds to B_0 nad intercept corrosponds to B_1 */
+        return [slope, intercept]
+
+
+
+
 
     }
 
