e046c772c04c164f819ae5212e372fbbd146f285
Weestack <Alexander@helius.dk>
2020-04-10 12:00:43 +0200

    bloated up filereader

--- prediction_lib/FileReader.js
+++ prediction_lib/FileReader.js
@@ -1,5 +1,6 @@
 "use strict";
-const fs = require("fs"); 
+const fs = require("fs");
+const path = require("path");
 
 
 class file_sanatiser {
@@ -18,22 +19,84 @@ class file_sanatiser {
 
 }
 
+
+
+
+
+
 class match_data extends file_sanatiser {
     /*
     * Factory class that should prepare CS:GO data for regression
-    * */
-    constructor(path){
+    */
+    constructor(absolute_path){
+        /* path is expected to be folder for all data */
         super();
-        this.path = path;
+        this.path = absolute_path;
 
-        let file = this.openfile();
+        //let files = fs.
+
+        this.data = this.filter_all_files();
+
+
+        /*let file = this.openfile();
         file = this.filter_file(file);
         
-        this.data = file;
+        this.data = file;*/
     }
 
-    filter_file(file){
-        let data = this.json_converter(file);
+    filter_all_files(){
+        let directory = path.join(__dirname, this.path);
+        let vector_data = [];
+        fs.readdir(directory, (error, files) => {
+
+            for (let i = 0; i < 1; i++){
+                //console.log(files[i])
+                let data = fs.readFileSync(this.path+"/"+files[i]);
+                let parsed_data = JSON.parse(data)
+                //console.log(parsed_data[0].last_matches)
+                let team_id = parsed_data["id"];
+                delete parsed_data["id"];
+                console.log(team_id)
+                vector_data[team_id] = this.filter_file(parsed_data);
+            }
+
+        })
+        return vector_data;
+    }
+
+    filter_file(parsed_data){
+        let data = Array();
+        let methods = [
+            "win_loose_for_team",
+            /*"best_n_worst_map_per_team",
+            "win_loose_between_team_and_current_opponent",
+            "most_used_weapons",
+            "most_deaths_to_weapons",
+            "mean_headshots",
+            "first_kill",
+            "time_sine_last_match",
+            "time_players_has_played_together_in_teams",
+            "mean_death_kill_ratio"*/
+
+
+
+        ]
+        let team_one = parsed_data[0];
+        let team_two = parsed_data[1];
+
+        console.log("test ",current_row)
+        for (let D_object in parsed_data[current_row]){
+            for (let m in methods){
+                console.log("d ",parsed_data[current_row])
+                let method_data = this[methods[m]](parsed_data[current_row][D_object], parsed_data[current_row]);
+                console.log("method data",method_data);
+                if (method_data !== undefined){
+                    data.push(method_data);
+                }
+            }
+        }
+
+
 
         return data;
     }
@@ -44,8 +107,9 @@ class match_data extends file_sanatiser {
 
     }
 
-    win_loose_for_team(name="win_loose", datarow){
+    win_loose_for_team(datarow, data_object_string_name){
         /* D1 */
+        console.log(data_object_string_name)
     }
 
 
@@ -86,3 +150,5 @@ class match_data extends file_sanatiser {
         /* D10 */
     }
 }
+
+new match_data("actual_data")
