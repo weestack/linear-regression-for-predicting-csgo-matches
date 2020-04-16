ce6caea9eff47bf75d97c8bb82fe7cb367282208
Weestack <Alexander@helius.dk>
2020-04-16 20:35:30 +0200

    cleaning up regression and filereader, to use for reqiure statements instead

--- prediction_lib/server.js
+++ prediction_lib/server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
 
-app.listen(8080);
+let filereader = require(path.resolve(__dirname, "./FileReader.js" ))
+
+console.log(filereader)
