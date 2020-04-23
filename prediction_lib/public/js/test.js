fe347ef5ece8788ede0fcc787a4fde53af836be0
Weestack <Alexander@helius.dk>
2020-04-23 12:20:39 +0200

    doing some initial cleanup and rewriting of d3 scatter plot matrix

--- prediction_lib/public/js/test.js
+++ prediction_lib/public/js/test.js
@@ -1,25 +1,26 @@
 
-var width = 960,
-    size = 230,
-    padding = 20;
 
-var x = d3.scaleLinear()
-    .range([padding / 2, size - padding / 2]);
+function get_scatter_plot_matrix_settings(){
+    let width = 760;
+    let size = 180;
+    let padding = 30;
 
-var y = d3.scaleLinear()
-    .range([size - padding / 2, padding / 2]);
+    /* d3 scalelinear is used to define the size of the linear boxes
+    * this is helpfull regarding using linear data on the form y= m*x + b */
+    let x = d3.scaleLinear().range( [padding/2, size-(padding/2)] );
+    let y = d3.scaleLinear().range( [size-(padding/2), padding/2 ] )
 
-var xAxis = d3.axisBottom()
-    .scale(x)
-    .ticks(6);
+    /* density off gitter */
+    let xAxis = d3.axisBottom().scale(x).ticks(10);
+    let yAxis = d3.axisLeft().scale(y).ticks(10);
 
-var yAxis = d3.axisLeft()
-    .scale(y)
-    .ticks(6);
+    /* color scheme to display residuals */
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    return [width, size, padding, x, y, xAxis, yAxis, color]
+}
+let [width, size, padding, x, y, xAxis, yAxis, color] = get_scatter_plot_matrix_settings();
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-d3.csv("data/out.csv", function(error, data) {
+d3.csv("data/flowers.csv", function(error, data) {
     if (error) throw error;
 
     var domainByTrait = {},
@@ -33,11 +34,6 @@ d3.csv("data/out.csv", function(error, data) {
     xAxis.tickSize(size * n);
     yAxis.tickSize(-size * n);
 
-    var brush = d3.brush()
-        .on("start", brushstart)
-        .on("brush", brushmove)
-        .on("end", brushend)
-        .extent([[0,0],[size,size]]);
 
     var svg = d3.select("body").append("svg")
         .attr("width", size * n + padding)
@@ -73,7 +69,6 @@ d3.csv("data/out.csv", function(error, data) {
         .attr("dy", ".71em")
         .text(function(d) { return d.x; });
 
-    cell.call(brush);
 
     function plot(p) {
         var cell = d3.select(this);
@@ -95,37 +90,6 @@ d3.csv("data/out.csv", function(error, data) {
             .attr("cy", function(d) { return y(d[p.y]); })
             .attr("r", 4)
             .style("fill", function(d) { return color(d.species); });
-    }
-
-    var brushCell;
-
-    // Clear the previously-active brush, if any.
-    function brushstart(p) {
-        if (brushCell !== this) {
-            d3.select(brushCell).call(brush.move, null);
-            brushCell = this;
-            x.domain(domainByTrait[p.x]);
-            y.domain(domainByTrait[p.y]);
-        }
-    }
-
-    // Highlight the selected circles.
-    function brushmove(p) {
-        var e = d3.brushSelection(this);
-        svg.selectAll("circle").classed("hidden", function(d) {
-            return !e
-                ? false
-                : (
-                    e[0][0] > x(+d[p.x]) || x(+d[p.x]) > e[1][0]
-                    || e[0][1] > y(+d[p.y]) || y(+d[p.y]) > e[1][1]
-                );
-        });
-    }
-
-    // If the brush is empty, select all circles.
-    function brushend() {
-        var e = d3.brushSelection(this);
-        if (e === null) svg.selectAll(".hidden").classed("hidden", false);
     }
 });
 
