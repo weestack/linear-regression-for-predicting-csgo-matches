1cb9c11953a674fbf7b7353b362964ce7d09ad8c
Weestack <Alexander@helius.dk>
2020-04-24 10:08:03 +0200

    cleanup continued in scatter matrix

--- prediction_lib/public/js/test.js
+++ prediction_lib/public/js/test.js
@@ -1,5 +1,3 @@
-
-
 function get_scatter_plot_matrix_settings(){
     let width = 760;
     let size = 180;
@@ -20,22 +18,30 @@ function get_scatter_plot_matrix_settings(){
 }
 let [width, size, padding, x, y, xAxis, yAxis, color] = get_scatter_plot_matrix_settings();
 
+/* Create D3 with csv. csv takes to inputs 1. csv file, 2 callback function */
+/* If no errors happend while reading in the csv file, then we use to callback */
+/* function to display the scatter matrix */
 d3.csv("data/flowers.csv", function(error, data) {
     if (error) throw error;
 
-    var domainByTrait = {},
-        traits = d3.keys(data[0]).filter(function(d) { return d !== "species"; }),
-        n = traits.length;
+    /* filter out text data, so only integers and floats are used to present the scatterplots */
+    let traits = d3.keys(data[0]).filter(function (data) {
+        return data !== "species";
+    })
+    let n = traits.length;
 
+    let domain_by_trait = {};
+    /* pack data into domain, by trait */
     traits.forEach(function(trait) {
-        domainByTrait[trait] = d3.extent(data, function(d) { return d[trait]; });
+        domain_by_trait[trait] = d3.extent(data, function(d) { return d[trait]; });
     });
+
 
     xAxis.tickSize(size * n);
     yAxis.tickSize(-size * n);
 
 
-    var svg = d3.select("body").append("svg")
+    let svg = d3.select("body").append("svg")
         .attr("width", size * n + padding)
         .attr("height", size * n + padding)
         .append("g")
@@ -46,16 +52,16 @@ d3.csv("data/flowers.csv", function(error, data) {
         .enter().append("g")
         .attr("class", "x axis")
         .attr("transform", function(d, i) { return "translate(" + (n - i - 1) * size + ",0)"; })
-        .each(function(d) { x.domain(domainByTrait[d]); d3.select(this).call(xAxis); });
+        .each(function(d) { x.domain(domain_by_trait[d]); d3.select(this).call(xAxis); });
 
     svg.selectAll(".y.axis")
         .data(traits)
         .enter().append("g")
         .attr("class", "y axis")
         .attr("transform", function(d, i) { return "translate(0," + i * size + ")"; })
-        .each(function(d) { y.domain(domainByTrait[d]); d3.select(this).call(yAxis); });
+        .each(function(d) { y.domain(domain_by_trait[d]); d3.select(this).call(yAxis); });
 
-    var cell = svg.selectAll(".cell")
+    let cell = svg.selectAll(".cell")
         .data(cross(traits, traits))
         .enter().append("g")
         .attr("class", "cell")
@@ -73,8 +79,8 @@ d3.csv("data/flowers.csv", function(error, data) {
     function plot(p) {
         var cell = d3.select(this);
 
-        x.domain(domainByTrait[p.x]);
-        y.domain(domainByTrait[p.y]);
+        x.domain(domain_by_trait[p.x]);
+        y.domain(domain_by_trait[p.y]);
 
         cell.append("rect")
             .attr("class", "frame")
